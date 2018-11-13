22ecdf9f4963a055646ad01c21663cae23eff87b
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2018-11-13 14:03:35 +0100

    Can you hear me?

---
+++ Source/JamTest/JamGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JamGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JAMTEST_API AJamGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
